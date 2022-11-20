a6e8bcf7cb7b94ba8a45b49d1728348f1d504b74
Matthew Li <matthewli2003@gmail.com>
2022-11-19 21:40:53 -0800

    Test

--- express/server.js
+++ express/server.js
@@ -6,18 +6,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const axios = require("axios"); 
 
-// const router = express.Router();
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Express.js!</h1>');
-//   res.end();
-// });
-// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-// router.post('/', (req, res) => res.json({ postBody: req.body }));
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-// app.use(bodyParser.json());
-// app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 app.get("/", (req, res) => {
   axios.get("https://quizapi.io/api/v1/questions?apiKey=GqUHpV0I96SwSw9eEHOll244Azgj8dLWdE2Oti4r&limit=10")
   .then(response => {
