99f2f8220fb2544788da1ac0b4d0b354af48cac0
Matthew Li <matthewli2003@gmail.com>
2022-11-19 22:19:39 -0800

    add service call

--- express/server.js
+++ express/server.js
@@ -4,13 +4,20 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const axios = require("axios"); 
+const axios = require("axios");
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  axios.get("https://quizapi.io/api/v1/questions?apiKey=GqUHpV0I96SwSw9eEHOll244Azgj8dLWdE2Oti4r&limit=10")
+  .then(response => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.header("Content-Type", "application/json");
+      res.send(response.data); 
+  })
+  .catch(error => {
+      console.log(error); 
+  })
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
